96d7eec7c8981f00a2530591b841d135fb5efe7a
hangSu123 <h6.su@connect.qut.edu.au>
2017-11-17 15:10:49 +1000

    add mysql connection

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Ifb399!@",
+  port:"3306",
+  database: "UPMS"
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+
+  con.connect(function(err) {
+	  if (err) res.render('login', { database: 'check your database connection' });
+	  res.render('login', { database: 'Database connected' });
+	});
+  
+
+
 });
 
 
 
+
+
+
+
+
+
+
+
+router.post('/test', function (req, res) {
+  var ip = req.connection.remoteAddress;
+  var time = Date();
+  var sql = "INSERT INTO connection (connectionIp, connectionTime) VALUES ('"+ip+"', '"+time+"')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send("1 record inserted");
+  });
+  
+})
 
 
 module.exports = router;
