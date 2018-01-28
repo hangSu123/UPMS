9ff86aad53ee284dd090b0ba9910786ef8269712
hangSu123 <h6.su@connect.qut.edu.au>
2018-01-28 22:45:17 +1000

    update sql

--- SQL/upms.sql
+++ SQL/upms.sql
@@ -3,15 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2017 at 03:10 AM
+-- Generation Time: Jan 28, 2018 at 01:43 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-DROP DATABASE IF EXISTS `upms`;
-CREATE DATABASE `upms`;
-USE `upms`;
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,7 +26,7 @@ USE `upms`;
 -- Table structure for table `announcements`
 --
 
-CREATE TABLE IF NOT EXISTS `announcements` (
+CREATE TABLE `announcements` (
   `Id` int(11) NOT NULL,
   `userId` varchar(50) NOT NULL,
   `title` text NOT NULL,
@@ -42,7 +39,165 @@ CREATE TABLE IF NOT EXISTS `announcements` (
 --
 
 INSERT INTO `announcements` (`Id`, `userId`, `title`, `content`, `date`) VALUES
-(1, '1', 'assignment 1', 'hello test 1', '2017-12-08');
+(1, '1', 'assignment 1', 'hello test 1', '2017-12-08'),
+(2, '1', 'assignment 2', 'Assignment to is due on the 23-12-2017, please refer to the submission doc for more detail ', '2017-12-13'),
+(3, '1', 'dxfsd', 'dfgfh', '2017-12-27'),
+(4, '1', 'sddg', 'dgdgfdfg', '2017-12-19'),
+(5, '1', 'test only', 'awdasd', '2017-12-19'),
+(6, '1', 'asdas', 'asdasdasda', '2017-11-23'),
+(7, '1', 'asdasda', 'ASDasd', '2017-11-07'),
+(8, '1', 'zxfcvbnm', 'asdftgtfrdes', '2018-01-17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application`
+--
+
+CREATE TABLE `application` (
+  `group_id` varchar(50) DEFAULT NULL,
+  `project_id` varchar(50) DEFAULT NULL,
+  `application_link` varchar(255) DEFAULT NULL,
+  `group_preference` int(11) DEFAULT NULL,
+  `supervisor_preference` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assignment`
+--
+
+CREATE TABLE `assignment` (
+  `assignment_id` varchar(255) NOT NULL,
+  `assignment_name` varchar(255) DEFAULT NULL,
+  `assignment_weight` decimal(5,2) DEFAULT NULL,
+  `submission_date` datetime DEFAULT NULL,
+  `CRA_link` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coordinator`
+--
+
+CREATE TABLE `coordinator` (
+  `cordinator_id` varchar(50) NOT NULL,
+  `cordinator_email_address` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `phone_number` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE `group` (
+  `group_id` varchar(50) NOT NULL,
+  `available_place` int(11) DEFAULT NULL,
+  `project_id` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting`
+--
+
+CREATE TABLE `meeting` (
+  `meeting_id` varchar(50) NOT NULL,
+  `type` enum('A','B','C') DEFAULT NULL,
+  `group_id` varchar(50) DEFAULT NULL,
+  `tutor_id` varchar(50) DEFAULT NULL,
+  `data_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(255) DEFAULT NULL,
+  `simple_description` varchar(255) DEFAULT NULL,
+  `space_available` int(11) NOT NULL,
+  `out_line_link` text,
+  `difficulty_level` enum('A','B','C') DEFAULT NULL,
+  `super_name` varchar(50) DEFAULT NULL,
+  `supervisor_email` varchar(225) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`project_id`, `project_name`, `simple_description`, `space_available`, `out_line_link`, `difficulty_level`, `super_name`, `supervisor_email`) VALUES
+(1, 'final test', 'thisis a test, test only ', 4, 'hello.pdf', 'C', 'Hang Su', 'suhangj123@gmail.com'),
+(2, 'test 2', 'i am your supervisor, i am Henry, handsome boy', 3, 'hello.pdf', 'B', 'Henry Chen', 'henry123@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `student_id` varchar(10) DEFAULT NULL,
+  `assignment_id` varchar(255) DEFAULT NULL,
+  `mark` decimal(5,2) DEFAULT NULL,
+  `submission_date` datetime DEFAULT NULL,
+  `feedback_link` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` varchar(10) NOT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `GPA` decimal(3,2) DEFAULT NULL,
+  `major` enum('CS','IS','UNKONW') DEFAULT NULL,
+  `group_id` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `supervisor_id` varchar(50) NOT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `phone_number` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor`
+--
+
+CREATE TABLE `tutor` (
+  `tutor_id` varchar(50) NOT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `phone_number` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -55,6 +210,70 @@ ALTER TABLE `announcements`
   ADD PRIMARY KEY (`Id`);
 
 --
+-- Indexes for table `application`
+--
+ALTER TABLE `application`
+  ADD KEY `FK,PK` (`group_id`,`project_id`);
+
+--
+-- Indexes for table `assignment`
+--
+ALTER TABLE `assignment`
+  ADD PRIMARY KEY (`assignment_id`);
+
+--
+-- Indexes for table `coordinator`
+--
+ALTER TABLE `coordinator`
+  ADD PRIMARY KEY (`cordinator_id`);
+
+--
+-- Indexes for table `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`group_id`),
+  ADD KEY `FK` (`project_id`);
+
+--
+-- Indexes for table `meeting`
+--
+ALTER TABLE `meeting`
+  ADD PRIMARY KEY (`meeting_id`),
+  ADD KEY `FK` (`group_id`,`tutor_id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`),
+  ADD KEY `FK` (`super_name`,`supervisor_email`);
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD KEY `FK,PK` (`student_id`,`assignment_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `FK` (`group_id`);
+
+--
+-- Indexes for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`supervisor_id`);
+
+--
+-- Indexes for table `tutor`
+--
+ALTER TABLE `tutor`
+  ADD PRIMARY KEY (`tutor_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,105 +281,12 @@ ALTER TABLE `announcements`
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-CREATE TABLE `Student` (
-  `student_id` varchar(10),
-  `email_address` varchar(50),
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `GPA` decimal(3,2),
-  `major` ENUM('CS','IS','UNKONW'),
-  `group_id` varchar(50),
-  PRIMARY KEY (`student_id`),
-  KEY `FK` (`group_id`)
-);
-
-CREATE TABLE `Application` (
-  `group_id` varchar(50),
-  `project_id` varchar(50),
-  `application_link` varchar(255),
-  `group_preference` int,
-  `supervisor_preference` int,
-  KEY `FK,PK` (`group_id`, `project_id`)
-);
-
-CREATE TABLE `Project` (
-  `project_id` varchar(50),
-  `project_name` varchar(255),
-  `simple_description_link` varchar(255),
-  `out_line_link` varchar(255),
-  `difficulty_level` ENUM('A','B','C'),
-  `conrdinator_id` varchar(50),
-  `supervisor_id` varchar(50),
-  PRIMARY KEY (`project_id`),
-  KEY `FK` (`conrdinator_id`, `supervisor_id`)
-);
-
-CREATE TABLE `Tutor` (
-  `tutor_id` varchar(50),
-  `email_address` varchar(50),
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `phone_number` int(10),
-  PRIMARY KEY (`tutor_id`)
-);
-
-CREATE TABLE `Meeting` (
-  `meeting_id` varchar(50),
-  `type` ENUM('A','B','C'),
-  `group_id` varchar(50),
-  `tutor_id` varchar(50),
-  `data_time` datetime,
-  PRIMARY KEY (`meeting_id`),
-  KEY `FK` (`group_id`, `tutor_id`)
-);
-
-CREATE TABLE `Coordinator` (
-  `cordinator_id` varchar(50),
-  `cordinator_email_address` varchar(50),
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `phone_number` int(10),
-  PRIMARY KEY (`cordinator_id`)
-);
-
-CREATE TABLE `Supervisor` (
-  `supervisor_id` varchar(50),
-  `email_address` varchar(50),
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `phone_number` int(10),
-  PRIMARY KEY (`supervisor_id`)
-);
-
-CREATE TABLE `Group` (
-  `group_id` varchar(50),
-  `available_place` int,
-  `project_id` varchar(50),
-  PRIMARY KEY (`group_id`),
-  KEY `FK` (`project_id`)
-);
-
-CREATE TABLE `Assignment` (
-  `assignment_id` varchar(255),
-  `assignment_name` varchar(255),
-  `assignment_weight` decimal(5,2),
-  `submission_date` datetime,
-  `CRA_link` varchar(255),
-  PRIMARY KEY (`assignment_id`)
-);
-
-CREATE TABLE `Result` (
-  `student_id` varchar(10),
-  `assignment_id` varchar(255),
-  `mark` decimal(5,2),
-  `submission_date` datetime,
-  `feedback_link` varchar(255),
-  KEY `FK,PK` (`student_id`, `assignment_id`)
-);
-
